47a24bd5253741fda884241c655d2c259a7fe7b6
LucasHzl <hezil.lucas@gmail.com>
2024-04-02 14:18:30 +0200

    config exemple

--- src/router.php
+++ src/router.php
@@ -1,3 +1,23 @@
 <?php
 
-$baseUri = '';
+$homeController = new HomeController();
+$loginController = new LoginController();
+$coursesController = new CoursesController();
+$rolesController = new RolesController();
+$promotionsController = new PromotionsController();
+$usersController = new UsersController();
+$notFoundController = new NotFoundController();
+
+
+
+switch ($highway) {
+        case HOME_PAGE:
+        //////
+        break;
+
+        default:
+        $notFoundController->notFoundPage;
+
+}
+
+?>
