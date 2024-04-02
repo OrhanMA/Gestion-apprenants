834aa802f5550bed4138566e6846d28227dd7d26
LucasHzl <hezil.lucas@gmail.com>
2024-04-02 16:02:30 +0200

    controller

--- src/router.php
+++ src/router.php
@@ -15,6 +15,26 @@ switch ($highway) {
         //////
         break;
 
+        case LOGIN_PAGE:
+        //////
+        break;
+
+        case COURSES_PAGE:
+        //////
+        break;
+
+        case ROLES_PAGE:
+        //////
+        break;
+
+        case PROMOTIONS_PAGE:
+        //////
+        break;
+
+        case USERS_PAGE:
+        //////
+        break;
+
         default:
         $notFoundController->notFoundPage;
 
