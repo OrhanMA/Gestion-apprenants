0222765bcbeaaa6423482e0985ec67a0e246ce44
Wabene cedric <cedric.wabenemayele.simplon@gmail.com>
2024-04-05 13:35:03 +0200

    front ced

--- src/Views/home.php
+++ src/Views/home.php
@@ -1,4 +1,4 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -39,8 +39,8 @@
     </ul>
     <h1>Cours du jour</h1>
 
-    <div class="container">
-        <div class="h1">
+    <div class="validePresence">
+        <div>
             <h1>DWWM3</h1>
 
             <p class="paraph">15 participants</p>
@@ -53,7 +53,66 @@
     </div>
 </body>
 
-</html> -->
+</html> 
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./../../css/style.css">
+
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">SIMPLON</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a>
+                    <span class="navbar-text">
+                        Déconnexion
+                    </span>
+                </a>
+            </div>
+        </nav>
+    </header>
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Promotions</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Utilisateurs</a>
+        </li>
+    </ul>
+    <h1>Cours du jour</h1>
+
+    <div class="signature">
+        <div class="recueillie">
+            <h1>DWWM3</h1>
+
+            <p class="paraph">15 participants</p>
+        </div>
+        <div>
+            <p class="para">01-01-2024</p>
+            
+            <button type="button" class="btn btn-secondary">Signature recueillie</button>        </div>
+    </div>
+</body>
+
+</html> 
+
 
 
 
@@ -99,9 +158,8 @@
     </ul>
     <h1>Cours du jour</h1>
 
-    <div class="backgroud">
         <div class="DWWM3">
-            <div class="">
+            <div class="cours">
                 <h2>DWWM3</h2>
                 <p>15 participants</p>
             </div>
@@ -110,11 +168,11 @@
                 <p class="para">01-01-2024</p>
                 <button type="button" class="btn btn-warning">Signatures en Cours</button>
             </div>
-        </div>
+        
     </div>
 
 
-    <div id="DWWM2">
+    <div class="DWWM2">
 
         <div>
             <h2>DWWM2</h2>
@@ -128,7 +186,7 @@
         </div>
     </div>
 
-    <div id="CDA">
+    <div class="CDA">
 
         <div>
             <h2>CDA</h2>
