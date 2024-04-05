f2e4b7082e8946473ab93aa98e8c93a8aec2ffae
DamienOrecchia38 <damien.orecchia.simplon@gmail.com>
2024-04-05 11:07:35 +0200

    Add - HTML and Bootstrap view of create and update  promotions pages

--- src/Views/home.php
+++ src/Views/home.php
@@ -5,23 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./styles/style.css">
+    <link rel="stylesheet" href="../../public/styles/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-    <h1>Titre de la page</h1>
-    <p class="descriptionParagraph">Sous-titre de la page</p>
-    <div class="pageContainer">
-        <!-- Ici injection du contenu (des "pages") avec Javascript -->
-    <!-- </div> -->
-    
-    <div class="container mt-3">
-        <h2>Toutes les promotions</h2>
-        <p>tableau des promotions de Simplon</p>
-        <div class="text-right mb-3">
-            <a class="btn btn-primary">Ajouter promotion</a>
+    <!-- <div class="container mt-3">
+        <div class="d-flex justify-content-between align-items-center">
+            <h2>Toutes les promotions</h2>
+            <a href="" class="btn btn-success font-weight-bold">Ajouter promotion</a>
         </div>
+        <p>tableau des promotions de Simplon</p>
         <table class="table">
             <thead>
                 <tr>
@@ -29,25 +23,110 @@
                     <th>Début</th>
                     <th>Fin</th>
                     <th>Places</th>
-                    <th>Actions</th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
+                <tr class="squareBullets table-header">
                     <td>DWWM 3</td>
                     <td>01-01-2024</td>
                     <td>01-12-2024</td>
                     <td>15</td>
                     <td>
-                        <a class="btn btn-info btn-sm">Voir</a>
-                        <a class="btn btn-warning btn-sm">Éditer</a>
-                        <a class="btn btn-danger btn-sm">Supprimer</a>
+                        <a class="btn btn-sm">Voir</a>
+                        <a class="btn btn-sm">Éditer</a>
+                        <a class="btn btn-sm">Supprimer</a>
+                    </td>
+                </tr>
+                <tr class="squareBullets">
+                    <td>DWWM 2</td>
+                    <td>01-01-2024</td>
+                    <td>01-12-2024</td>
+                    <td>15</td>
+                    <td>
+                        <a class="btn btn-sm">Voir</a>
+                        <a class="btn btn-sm">Éditer</a>
+                        <a class="btn btn-sm">Supprimer</a>
+                    </td>
+                </tr>
+                <tr class="squareBullets">
+                    <td>CDA</td>
+                    <td>01-01-2024</td>
+                    <td>01-12-2024</td>
+                    <td>12</td>
+                    <td>
+                        <a class="btn btn-sm">Voir</a>
+                        <a class="btn btn-sm">Éditer</a>
+                        <a class="btn btn-sm">Supprimer</a>
+                    </td>    
+                </tr>
+                <tr class="squareBullets">
+                    <td>CDA list</td>
+                    <td>01-01-2024</td>
+                    <td>01-12-2024</td>
+                    <td>12</td>
+                    <td>
+                        <a class="btn btn-sm">Voir</a>
+                        <a class="btn btn-sm">Éditer</a>
+                        <a class="btn btn-sm">Supprimer</a>
                     </td>
                 </tr>
             </tbody>
         </table>
-    </div>
-    <script src="./../src/scripts/form.js"></script>
+    </div> -->
+
+    <!-- <div class="container mt-5">
+        <h2 class="mb-3">Création d'une promotion</h2>
+        <form>
+            <div class="form-group mb-3">
+                <label for="nomPromotion">Nom de la promotion</label>
+                <input type="text" class="form-control" id="nomPromotion" placeholder="Entrez le nom de la promotion">
+            </div>
+            <div class="form-group mb-3">
+                <label for="dateDebut">Date de début</label>
+                <input type="date" class="form-control" id="dateDebut">
+            </div>
+            <div class="form-group mb-3">
+                <label for="dateFin">Date de fin</label>
+                <input type="date" class="form-control" id="dateFin">
+            </div>
+            <div class="form-group mb-3">
+                <label for="placesDisponibles">Place(s) disponible(s)</label>
+                <input type="number" class="form-control" id="placesDisponibles" placeholder="Nombre de places disponibles">
+            </div>
+            <button type="submit" class="btn btn-primary">Sauvegarder</button>
+        </form>
+    </div> -->
+
+    <!-- <div class="container mt-5">
+        <h2>Édition de la promotion DWWM3</h2>
+        <p>Les changements appliqués sont définitifs</p>
+        <form>
+            <div class="form-group mb-3">
+                <label for="nomPromotion">Nom de la promotion</label>
+                <input type="text" class="form-control" id="nomPromotion" value="DWWM3">
+            </div>
+            <div class="form-group mb-3">
+                <label for="dateDebut">Date de début</label>
+                <input type="date" class="form-control" id="dateDebut" value="2024-01-01">
+            </div>
+            <div class="form-group mb-3">
+                <label for="dateFin">Date de fin</label>
+                <input type="date" class="form-control" id="dateFin" value="2024-12-01">
+            </div>
+            <div class="form-group mb-3">
+                <label for="placesDisponibles">Place(s) disponible(s)</label>
+                <input type="number" class="form-control" id="placesDisponibles" value="15">
+            </div>
+            <div class="form-group mb-3">
+                <button type="button" class="btn btn-danger">Supprimer</button>
+                <button type="submit" class="btn btn-primary">Sauvegarder</button>
+            </div>
+        </form>
+    </div> -->
+    
+    <script src="./../scripts/promotion.js"></script>
+    <script src="./../scripts/form.js"></script>
 </body>
 
 </html>
