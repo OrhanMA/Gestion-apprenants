10121180b271d3eab1783efdfa8ae4a3c0bed9b1
Wabene cedric <cedric.wabenemayele.simplon@gmail.com>
2024-04-05 12:06:27 +0200

    front modified

--- js/form.js
+++ js/form.js
@@ -1,33 +1,109 @@
 function createForm(inputsData, action, target, id) {
-    const form = document.createElement('form');
-    form.action = action;
-    form.method = 'POST';
-    form.classList.add("mb-3");
+  const form = document.createElement("form");
+  form.action = action;
+  form.method = "POST";
+  form.classList.add("mb-3");
 
-    inputsData.forEach(inputData => {
-        const input = document.createElement('input');
-        input.type = inputData.type;
-        input.name = inputData.name;
-        input.classList.add(inputData.classe);
-        input.id = inputData.id
-        input.placeholder = inputData.placeholder;
-        form.appendChild(input);
-    });
+  inputsData.forEach((inputData) => {
+    const input = document.createElement("input");
+    input.type = inputData.type;
+    input.name = inputData.name;
+    input.classList.add(inputData.classe);
+    input.id = inputData.id;
+    input.placeholder = inputData.placeholder;
+    form.appendChild(input);
+  });
 
-    const buttonSubmit = document.createElement('input');
-    buttonSubmit.type = 'submit';
-    buttonSubmit.value = 'Submit';
-    buttonSubmit.classList.add("btn", "btn-primary");
-    form.appendChild(buttonSubmit);
+  const buttonSubmit = document.createElement("input");
+  buttonSubmit.type = "submit";
+  buttonSubmit.value = "Submit";
+  buttonSubmit.classList.add("btn", "btn-primary");
+  form.appendChild(buttonSubmit);
 
-    target.appendChild(form);
+  target.appendChild(form);
 }
 
-const body = document.querySelector('body');
+const body = document.querySelector("body");
 const inputsData = [
-    { type: 'text', name: 'userName', placeholder: 'Name', classe : 'form-label', id : 'userName' },
-    { type: 'email', name: 'userEmail', placeholder: 'Email', classe : 'form-label', id : 'userName'}
-    
+  {
+    type: "email",
+    name: "userEmail",
+    placeholder: "Email",
+    classe: "form-label",
+    id: "userName",
+  },
 ];
 
-createForm(inputsData, 'http://localhost:8888/projets/Gestion-apprenants/public/users', body);
+createForm(
+  inputsData,
+  "http://localhost:8888/projets/Gestion-apprenants/public/users",
+  body
+);
+
+
+const navbar = createElementClasses("nav", [
+  "navbar navbar-expand-lg navbar-light bg-light",
+]);
+
+
+const navbarContainer = createElementClasses("div", [
+  "container-fluid",
+]);
+navbar.appendChild(navbarContainer);
+
+const navbarBrand = createElementClasses("a", ["navbar-brand"]);
+navbarBrand.textContent = "SIMPLON";
+navbarBrand.href = "#";
+navbarContainer.appendChild(navbarBrand);
+
+const navbarToggler = createElementClasses("button", [
+  "navbar-toggler",
+]);
+navbarToggler.type = "button";
+navbarToggler.setAttribute("data-bs-toggle", "collapse");
+navbarToggler.setAttribute("data-bs-target", "#navbarNav");
+navbarToggler.setAttribute("aria-controls", "navbarNav");
+navbarToggler.setAttribute("aria-expanded", "false");
+navbarToggler.setAttribute("aria-label", "Toggle navigation");
+navbarContainer.appendChild(navbarToggler);
+
+const navbarTogglerIcon = createElementClasses("span", [
+  "navbar-toggler-icon",
+]);
+navbarToggler.appendChild(navbarTogglerIcon);
+
+const navbarCollapse = createElementClasses("div", [
+  "collapse",
+  "navbar-collapse",
+]);
+navbarCollapse.id = "navbarNav";
+navbarContainer.appendChild(navbarCollapse);
+
+const navbarNav = createElementClasses("ul", ["navbar-nav"]);
+navbarCollapse.appendChild(navbarNav);
+
+const navItem = createElementClasses("li", ["nav-item"]);
+navbarNav.appendChild(navItem);
+
+const navLink = createElementClasses("a", ["nav-link"]);
+navLink.textContent = "Accueil";
+navLink.href = "#";
+navItem.appendChild(navLink);
+
+const navItem2 = createElementClasses("li", ["nav-item"]);
+navbarNav.appendChild(navItem2);
+
+const navLink2 = createElementClasses("a", ["nav-link"]);
+navLink2.textContent = "Promotions";
+navLink2.href = "#";
+navItem2.appendChild(navLink2);
+
+const navbarText = createElementClasses("span", ["navbar-text"]);
+navbarText.textContent = "Déconnexion";
+navbarContainer.appendChild(navbarText);
+
+const conteneur = document.querySelector("header");
+conteneur.appendChild(navbar);
+
+
+
--- src/Views/home.php
+++ src/Views/home.php
@@ -1,17 +1,146 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
-    rel="stylesheet" 
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
-    crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/style.cs">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./../../css/style.css">
+
 </head>
+
 <body>
-    <h1 id ="titre"></h1>
- <script type="module" src="/js/form.js"></script>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">SIMPLON</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a>
+                    <span class="navbar-text">
+                        Déconnexion
+                    </span>
+                </a>
+            </div>
+        </nav>
+    </header>
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Promotions</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Utilisateurs</a>
+        </li>
+    </ul>
+    <h1>Cours du jour</h1>
+
+    <div class="container">
+        <div class="h1">
+            <h1>DWWM3</h1>
+
+            <p class="paraph">15 participants</p>
+        </div>
+        <div>
+            <p class="para">01-01-2024</p>
+            
+            <button type="button" class="btn btn-primary">Valide présence</button>
+        </div>
+    </div>
 </body>
+
+</html> -->
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./../../css/style.css">
+
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">SIMPLON</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a>
+                    <span class="navbar-text">
+                        Déconnexion
+                    </span>
+                </a>
+            </div>
+        </nav>
+    </header>
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Promotions</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Utilisateurs</a>
+        </li>
+    </ul>
+    <h1>Cours du jour</h1>
+
+    <div class="backgroud">
+        <div class="DWWM3">
+            <div class="">
+                <h2>DWWM3</h2>
+                <p>15 participants</p>
+            </div>
+            <div>
+
+                <p class="para">01-01-2024</p>
+                <button type="button" class="btn btn-warning">Signatures en Cours</button>
+            </div>
+        </div>
+    </div>
+
+
+    <div id="DWWM2">
+
+        <div>
+            <h2>DWWM2</h2>
+            <p class="paraph">15 participants</p>
+        </div>
+
+        <div>
+            <p class="para">01-01-2024</p>
+            <button type="button" class="btn btn-success">Signatures recueillies</button>
+
+        </div>
+    </div>
+
+    <div id="CDA">
+
+        <div>
+            <h2>CDA</h2>
+            <p class="paraph">12 participants</p>
+        </div>
+
+        <div>
+            <p class="para">01-01-2024</p>
+            <button type="button" class="btn btn-danger">Signatures manquantes</button>
+        </div>
+    </div>
+
+</body>
+
 </html>
